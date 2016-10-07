42736c04375534fc6405a58b94358d1cd1ebf28f
Neil Kirsopp <neil@lshift.net>
2016-10-07 14:52:58 +0100

    Kill has-key, separate running from -main

--- src/coney/core.clj
+++ src/coney/core.clj
@@ -1,6 +1,7 @@
 (ns coney.core
   (:gen-class)
   (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
             [org.httpkit.client :as http]
             [cheshire.core :as cheshire]
             [coney.rabbit-password :as rp]
@@ -90,17 +91,15 @@
   (str "Usage: \n" options-summary))
 
 (defn file-exists [path]
-  (.exists (clojure.java.io/as-file path)))
+  (.exists (io/as-file path)))
 
-(defn parse-file [filetype fname data]
+(defn parse-file [filetype fname]
   (case filetype
     :edn (try
-           (edn/read-string data)
+           (edn/read-string (slurp fname))
            (catch RuntimeException e (exit 1 (format "Bad EDN file '%s'" fname))))
-    :json (cheshire/parse-string data true)
+    :json (cheshire/parse-string (slurp fname) true)
     (exit 1 (error-msg [(format "Don't know file format '%s'" (name filetype))]))))
-(defn has-key [coll key]
-  (and (not (nil? (keys coll))) (.contains (keys coll) key)))
 
 (defn sync-config-multiple-vhost-generic [hash-func sync-func kind existing-for-vhost wanted sync-keys]
   (let [wanted-vals (vals wanted)
@@ -112,6 +111,75 @@
 (defn sync-config-multiple-vhost [kind existing-for-vhost wanted sync-keys]
   (sync-config-multiple-vhost-generic get-name-from-hash sync-config kind existing-for-vhost wanted sync-keys))
 
+(defn run-with-config [filetype fname]
+    (let [config (parse-file filetype fname)
+          existing-users (get-names-from-api "users")
+          wanted-users (get-names-from-hash :users config)
+          existing-vhosts (map #(keyword (:name %)) (-> @(http/get (str @root "vhosts") (core-params)) :body (cheshire/decode true)))
+          wanted-vhosts (get-names-from-hash :vhosts config)]
+      (doseq [user (keys wanted-users) :let [wanted-password (:password (user wanted-users))]]
+        (if (nil? wanted-password)
+          (exit 1 (format "missing password for user '%s'" (name user))))
+        (if (or (not (contains? existing-users user)) (not (rp/check-rabbit-password wanted-password (:password_hash (user existing-users)))))
+          (do
+            (println (format "missing user '%s'" (name user)))
+            (expected-code @(http/put
+                              (str @root "users/" (name user))
+                              (merge (core-params) {:body (cheshire/encode {:tags "" :password wanted-password})}))
+                           204))))
+      (doseq [vhost (keys wanted-vhosts)]
+        (if (not (.contains existing-vhosts vhost))
+          (do
+            (println (format "missing vhost '%s'" (name vhost)))
+            (expected-code @(http/put
+                              (str @root "vhosts/" (name vhost))
+                              (core-params))
+                           204)))
+        (let [vhost-encoded (http/url-encode (name vhost))
+              vhost-config (vhost wanted-vhosts)]
+          (sync-config "permissions"
+                       (get-users-from-api "permissions")
+                       (get-users-from-hash :permissions vhost-config) vhost
+                       [:configure :write :read])
+          (sync-config "queues"
+                       (get-names-from-api (str "queues/" vhost-encoded))
+                       (get-names-from-hash :queues vhost-config) vhost
+                       [:arguments :durable :auto-delete])
+          (sync-config "exchanges"
+                       (get-names-from-api (str "exchanges/" vhost-encoded))
+                       (get-names-from-hash :exchanges vhost-config) vhost
+                       [:arguments :internal :type :auto_delete :durable])
+          (sync-bindings "bindings"
+                         (get-x-from-api get-bindings-from-hash "bindings")
+                         (apply merge (map get-bindings-from-hash (:bindings vhost-config)))
+                         vhost
+                         [:destination :destination_type :arguments :routing_key :source])))
+      (when (:permissions config)
+        (sync-config-multiple-vhost-generic
+          get-user-from-hash sync-config
+          "permissions"
+          (fn [& _] (get-users-from-api "permissions"))
+          (get-users-from-hash :permissions config)
+          [:configure :write :read]))
+      (when (:queues config)
+        (sync-config-multiple-vhost "queues"
+                                    #(get-names-from-api (str "queues/" %))
+                                    (get-names-from-hash :queues config)
+                                    [:arguments :durable :auto-delete]))
+      (when (:exchanges config)
+        (sync-config-multiple-vhost "exchanges"
+                                    #(get-names-from-api (str "exchanges/" %))
+                                    (get-names-from-hash :exchanges config)
+                                    [:arguments :internal :type :auto_delete :durable]))
+      (when (:bindings config)
+        (sync-config-multiple-vhost-generic
+          get-bindings-from-hash sync-bindings
+          "bindings"
+          #(get-x-from-api get-bindings-from-hash (str "bindings/" %))
+          (apply merge (map get-bindings-from-hash (:bindings config)))
+          [:destination :destination_type :arguments :routing_key :source]))
+      (println "All done")))
+
 (defn -main
   [& args]
   (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
@@ -122,71 +190,5 @@
       errors (exit 1 (error-msg errors))
       (not= (count arguments) 1) (exit 1 (format "Need a single file argument, but got %d arguments" (count arguments)))
       (not (file-exists (first arguments))) (exit 1 (error-msg [(format "No such file '%s'" (first arguments))]))
-      :default (let [fname (first arguments)
-                     config (parse-file (:filetype options) fname (slurp fname))
-                     existing-users (get-names-from-api "users")
-                     wanted-users (get-names-from-hash :users config)
-                     existing-vhosts (map #(keyword (:name %)) (-> @(http/get (str @root "vhosts") (core-params)) :body (cheshire/decode true)))
-                     wanted-vhosts (get-names-from-hash :vhosts config)]
-                 (doseq [user (keys wanted-users) :let [wanted-password (:password (user wanted-users))]]
-                   (if (nil? wanted-password)
-                     (exit 1 (format "missing password for user '%s'" (name user))))
-                   (if (or (not (contains? existing-users user)) (not (rp/check-rabbit-password wanted-password (:password_hash (user existing-users)))))
-                     (do
-                       (println (format "missing user '%s'" (name user)))
-                       (expected-code @(http/put
-                                        (str @root "users/" (name user))
-                                        (merge (core-params) {:body (cheshire/encode {:tags "" :password wanted-password})}))
-                                      204))))
-                 (doseq [vhost (keys wanted-vhosts)]
-                   (if (not (.contains existing-vhosts vhost))
-                     (do
-                       (println (format "missing vhost '%s'" (name vhost)))
-                       (expected-code @(http/put
-                                        (str @root "vhosts/" (name vhost))
-                                        (core-params))
-                                      204)))
-                   (let [vhost-encoded (http/url-encode (name vhost))
-                         vhost-config (vhost wanted-vhosts)]
-                     (sync-config "permissions"
-                                  (get-users-from-api "permissions")
-                                  (get-users-from-hash :permissions vhost-config) vhost
-                                  [:configure :write :read])
-                     (sync-config "queues"
-                                  (get-names-from-api (str "queues/" vhost-encoded))
-                                  (get-names-from-hash :queues vhost-config) vhost
-                                  [:arguments :durable :auto-delete])
-                     (sync-config "exchanges"
-                                  (get-names-from-api (str "exchanges/" vhost-encoded))
-                                  (get-names-from-hash :exchanges vhost-config) vhost
-                                  [:arguments :internal :type :auto_delete :durable])
-                     (sync-bindings "bindings"
-                                    (get-x-from-api get-bindings-from-hash "bindings")
-                                    (apply merge (map get-bindings-from-hash (:bindings vhost-config)))
-                                    vhost
-                                    [:destination :destination_type :arguments :routing_key :source])))
-                 (if (has-key config :permissions)
-                   (sync-config-multiple-vhost-generic
-                    get-user-from-hash sync-config
-                    "permissions"
-                    (fn [& _] (get-users-from-api "permissions"))
-                    (get-users-from-hash :permissions config)
-                    [:configure :write :read]))
-                 (if (has-key config :queues)
-                   (sync-config-multiple-vhost "queues"
-                                               #(get-names-from-api (str "queues/" %))
-                                               (get-names-from-hash :queues config)
-                                               [:arguments :durable :auto-delete]))
-                 (if (has-key config :exchanges)
-                   (sync-config-multiple-vhost "exchanges"
-                                               #(get-names-from-api (str "exchanges/" %))
-                                               (get-names-from-hash :exchanges config)
-                                               [:arguments :internal :type :auto_delete :durable]))
-                 (if (has-key config :bindings)
-                   (sync-config-multiple-vhost-generic
-                    get-bindings-from-hash sync-bindings
-                    "bindings"
-                    #(get-x-from-api get-bindings-from-hash (str "bindings/" %))
-                    (apply merge (map get-bindings-from-hash (:bindings config)))
-                    [:destination :destination_type :arguments :routing_key :source]))
-                 (println "All done")))))
+      :default (run-with-config (:filetype options) (first arguments)))))
+
